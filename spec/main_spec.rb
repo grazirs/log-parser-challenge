740ad4a05c0567d93175bcc8d56ea3c57503e879
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-01-18 01:07:56 -0300

    test: add automated tests

---
+++ spec/main_spec.rb
@@ -0,0 +1,20 @@
+require "main"
+describe LogParser do
+    context "call print_first_line" do
+        it "prints first line" do
+            log_parser = LogParser.new("games.log")
+            expect do
+                log_parser.print_first_line
+            end.to output("  0:00 ------------------------------------------------------------\n").to_stdout
+        end
+    end
+
+    context "games.log does not exist" do
+        it "prints file not found" do
+            log_parser = LogParser.new("game.log")
+            expect do
+                log_parser.print_first_line
+            end.to output("File not found\n").to_stdout
+        end
+    end
+end
