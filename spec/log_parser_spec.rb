5a12d533b0b6aac0b687a9f245b0c8317928e0c5
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-01-18 15:37:23 -0300

    add game_test.log

--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -2,7 +2,7 @@ require "log_parser"
 describe LogParser do
     context "call print_first_line" do
         it "prints first line" do
-            log_parser = LogParser.new("games.log")
+            log_parser = LogParser.new("spec/fixtures/game_test.log")
             expect do
                 log_parser.print_first_line
             end.to output("  0:00 ------------------------------------------------------------\n").to_stdout
