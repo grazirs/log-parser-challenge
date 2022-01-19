7ec8215e76e45aacec8e751fa8849ef34ef07b58
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-01-19 19:50:53 -0300

    refactor: LogParser and test

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -2,18 +2,15 @@ require "json"
 class LogParser
     def initialize(file_path)
         @file_path = file_path
+        raise "File not found" unless File.exists?(@file_path)
     end
 
     def get_log_data
-        begin
-            file = File.open(@file_path)
-            file_data = file.readlines.map(&:chomp)
-            lines_number = file_data.length
-            file.close
-            data = {:lines => lines_number}
-            '"%{file_name}": %{json}' % {file_name:File.basename(@file_path), json:JSON.pretty_generate(data)}
-        rescue => exception
-            puts "File not found"
-        end
+      file = File.open(@file_path)
+      file_data = file.readlines.map(&:chomp)
+      lines_number = file_data.length
+      file.close
+      data = {:lines => lines_number}
+      '"%{file_name}": %{json}' % {file_name:File.basename(@file_path), json:JSON.pretty_generate(data)}
     end
 end
