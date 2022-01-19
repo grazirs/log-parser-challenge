a4dcaf56628e89e329fa156b5b356010cceaaf5f
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-01-19 13:11:09 -0300

    refactor: LogParser and tests

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,17 +1,14 @@
 class LogParser
-    def initialize(file_name)
-        @file_name = file_name
-    end
+  def initialize(file_name)
+    @file_name = file_name
+    raise "File not found" unless File.exists?(@file_name)
+  end
 
-    def get_first_line
-        begin
-            file = File.open(@file_name)
-            file_data = file.readlines.map(&:chomp)
-            first_line = file_data[0]
-            file.close
-            first_line
-        rescue => exception
-            puts "File not found"
-        end
-    end
+  def get_first_line
+    file = File.open(@file_name)
+    file_data = file.readlines.map(&:chomp)
+    first_line = file_data[0]
+    file.close
+    first_line
+  end
 end
