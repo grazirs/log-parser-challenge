b0a7c8444db8cb68592d8a3e652a318ec76e2763
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-01-20 21:00:40 -0300

    add players name to get_log_data

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -9,8 +9,31 @@ class LogParser
       file = File.open(@file_path)
       file_data = file.readlines.map(&:chomp)
       lines_number = file_data.length
+      players = []
+      file_data.each do |line|
+        if line.match(/ \d{1,2}:\d{1,2} Kill: \d+ \d+ \d+: .+ killed .+by .+/)
+          player_one = line[line.rindex(":")+2..line.index("killed")-2]
+          player_two = line[line.index("killed")+7..line.rindex("by")-2]
+          players.push(player_one)
+          players.push(player_two)
+        end
+        if line.match(/ \d{1,2}:\d{1,2} ClientUserinfoChanged: \d+ n\\.+\\t\\\d+\\model/)
+          player = line[line.index("n\\")+2..line.index("\\t")-1]
+          players.push(player)
+        end
+      end
       file.close
-      data = {:lines => lines_number}
+      data = {:lines => lines_number, :players => players.uniq}
       '"%{file_name}": %{json}' % {file_name:File.basename(@file_path), json:JSON.pretty_generate(data)}
     end
 end
+
+
+
+# const data = ['ciao 1', 'amore', 'dove', 'ciao2', 'mare'];
+
+# data.foreach((element){
+#   if (element.contains('ciao'))
+#     console.log(element);
+  
+# });
--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
 require_relative "lib/log_parser"
 log_parser = LogParser.new("games.log")
 data = log_parser.get_log_data
-puts data 
+puts data
