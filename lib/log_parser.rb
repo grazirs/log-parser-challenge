beb10f1becaf32cc0c940107269d5ee47f35e1de
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-01-21 20:25:57 -0300

    refactor: LogParser and add test

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,29 +1,42 @@
 require "json"
 class LogParser
-    def initialize(file_path)
-        @file_path = file_path
-        raise "File not found" unless File.exists?(@file_path)
-    end
+  def initialize(file_path)
+    @file_path = file_path
+    raise "File not found" unless File.exists?(@file_path)
+    file = File.open(@file_path)
+    @file_data = file.readlines.map(&:chomp)
+    file.close
+  end
 
-    def get_log_data
-      file = File.open(@file_path)
-      file_data = file.readlines.map(&:chomp)
-      lines_number = file_data.length
-      players = []
-      file_data.each do |line|
-        if line.match(/ \d{1,2}:\d{1,2} Kill: \d+ \d+ \d+: .+ killed .+by .+/)
-          player_one = line[line.rindex(":")+2..line.index("killed")-2]
-          player_two = line[line.index("killed")+7..line.rindex("by")-2]
-          players.push(player_one)
-          players.push(player_two)
-        end
-        if line.match(/ \d{1,2}:\d{1,2} ClientUserinfoChanged: \d+ n\\.+\\t\\\d+\\model/)
-          player = line[line.index("n\\")+2..line.index("\\t")-1]
-          players.push(player)
-        end
+  def get_first_line
+    first_line = @file_data[0]
+  end
+
+  def get_log_data
+    players = []
+    @file_data.each do |line|
+      if line.match(/ \d{1,2}:\d{1,2} Kill: \d+ \d+ \d+: .+ killed .+by .+/)
+        player_one = line[line.rindex(":")+2..line.index("killed")-2]
+        player_two = line[line.index("killed")+7..line.rindex("by")-2]
+        players.push(player_one)
+        players.push(player_two)
       end
-      file.close
-      data = {:lines => lines_number, :players => players.uniq}
-      '"%{file_name}": %{json}' % {file_name:File.basename(@file_path), json:JSON.pretty_generate(data)}
+      if line.match(/ \d{1,2}:\d{1,2} ClientUserinfoChanged: \d+ n\\.+\\t\\\d+\\model/)
+        player = line[line.index("n\\")+2..line.index("\\t")-1]
+        players.push(player)
+      end
     end
+    data = {:lines => self.file_length, :players => players.uniq}
+    '"%{file_name}": %{json}' % {file_name: self.file_name, json: JSON.pretty_generate(data)}
+  end
+
+  private
+
+  def file_length
+    @file_data.length
+  end
+  
+  def file_name
+    File.basename(@file_path)
+  end
 end
--- spec/lib/log_parser_spec.rb
+++ spec/lib/log_parser_spec.rb
@@ -1,19 +1,33 @@
 require "log_parser"
 describe LogParser do
-  context "call print_log_data" do
-    it "prints lines number" do
+  describe "#get_first_line" do
+    context "call get_first_line" do
+      it "gets first line" do
       log_parser = LogParser.new("spec/fixtures/game_test.log")
-      output = log_parser.get_log_data
-      expect(output).to start_with('"game_test.log":')
-      json_data = JSON.parse(output[17, output.length-1])
-      expect(json_data['lines']).to eq(52)
-      expect(json_data['players']).to eq(["Isgalamido", "<world>", "Dono da Bola", "Mocinha", "UnnamedPlayer"])
+      first_line = log_parser.get_first_line
+      expect(first_line).to eq("  0:00 ------------------------------------------------------------")
+      end
     end
   end
 
-  context "games.log does not exist" do
-    it "print file not found" do
-      expect { LogParser.new("game.log") }.to raise_error("File not found")
+  describe "#when file does not exist" do
+    context "games.log does not exist" do
+      it "print file not found" do
+        expect { LogParser.new("game.log") }.to raise_error("File not found")
+      end
+    end
+  end
+  
+  describe "#get_log_data" do
+    context "call print_log_data" do
+      it "prints lines number" do
+        log_parser = LogParser.new("spec/fixtures/game_test.log")
+        output = log_parser.get_log_data
+        expect(output).to start_with('"game_test.log":')
+        json_data = JSON.parse(output[17, output.length-1])
+        expect(json_data['lines']).to eq(52)
+        expect(json_data['players']).to eq(["Isgalamido", "<world>", "Dono da Bola", "Mocinha", "UnnamedPlayer"])
+      end
     end
   end
 end
