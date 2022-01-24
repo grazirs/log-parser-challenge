e673d36e1f7e9ccecce89d1f93daadbd9a509a33
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-01-24 16:54:22 -0300

    refactor, apply rubocop  and simplecov

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,38 +1,31 @@
-require "json"
+# frozen_string_literal: true
+
+require 'json'
+# read and collect information of log.file
 class LogParser
   def initialize(file_path)
     @file_path = file_path
-    raise "File not found" unless File.exists?(@file_path)
+    raise 'File not found' unless File.exist?(@file_path)
+
     file = File.open(@file_path)
     @file_data = file.readlines.map(&:chomp)
     file.close
   end
 
-  def get_first_line
-    first_line = @file_data[0]
+  def first_line
+    @file_data[0]
   end
 
-  def get_log_data
+  def log_data
     players = []
     kills = {}
     total_kills = 0
     @file_data.each do |line|
-      if line.match(/ \d{1,2}:\d{1,2} Kill: \d+ \d+ \d+: .+ killed .+by .+/)
-        player_one = line[line.rindex(":")+2..line.index("killed")-2]
-        player_two = line[line.index("killed")+7..line.rindex("by")-2]
-        players.push(player_one)
-        players.push(player_two)
-        kills[player_one] = (kills[player_one] || 0) +1
-        total_kills = total_kills + 1
-
-      end
-      if line.match(/ \d{1,2}:\d{1,2} ClientUserinfoChanged: \d+ n\\.+\\t\\\d+\\model/)
-        player = line[line.index("n\\")+2..line.index("\\t")-1]
-        players.push(player)
-      end
+      total_kills += player_kills(line, players, kills)
+      player_info(line, players)
     end
-    data = {:lines => self.file_length, :players => players.uniq, :kills => kills, :total_kills => total_kills}
-    '"%{file_name}": %{json}' % {file_name: self.file_name, json: JSON.pretty_generate(data)}
+    data = { lines: file_length, players: players.uniq, kills:, total_kills: }
+    format('"%<file_name>s": %<json>s', file_name:, json: JSON.pretty_generate(data))
   end
 
   private
@@ -40,8 +33,26 @@ class LogParser
   def file_length
     @file_data.length
   end
-  
+
   def file_name
     File.basename(@file_path)
+  end
+
+  def player_kills(line, players, kills)
+    return 0 unless line.match(/ \d{1,2}:\d{1,2} Kill: \d+ \d+ \d+: .+ killed .+by .+/)
+
+    player_one = line[line.rindex(':') + 2..line.index('killed') - 2]
+    player_two = line[line.index('killed') + 7..line.rindex('by') - 2]
+    players.push(player_one)
+    players.push(player_two)
+    kills[player_one] = (kills[player_one] || 0) + 1
+    1
+  end
+
+  def player_info(line, players)
+    return unless line.match(/ \d{1,2}:\d{1,2} ClientUserinfoChanged: \d+ n\\.+\\t\\\d+\\model/)
+
+    player = line[line.index('n\\') + 2..line.index('\\t') - 1]
+    players.push(player)
   end
 end
--- spec/lib/log_parser_spec.rb
+++ spec/lib/log_parser_spec.rb
@@ -1,33 +1,38 @@
-require "log_parser"
+# frozen_string_literal: true
+
+require 'simplecov'
+SimpleCov.start
+require 'log_parser'
+
 describe LogParser do
-  describe "#get_first_line" do
-    context "call get_first_line" do
-      it "gets first line" do
-      log_parser = LogParser.new("spec/fixtures/game_test.log")
-      first_line = log_parser.get_first_line
-      expect(first_line).to eq("  0:00 ------------------------------------------------------------")
+  describe '#first_line' do
+    context 'call first_line' do
+      it 'gets first line' do
+        log_parser = LogParser.new('spec/fixtures/game_test.log')
+        first_line = log_parser.first_line
+        expect(first_line).to eq('  0:00 ------------------------------------------------------------')
       end
     end
   end
 
-  describe "#when file does not exist" do
-    context "games.log does not exist" do
-      it "print file not found" do
-        expect { LogParser.new("game.log") }.to raise_error("File not found")
+  describe '#when file does not exist' do
+    context 'games.log does not exist' do
+      it 'print file not found' do
+        expect { LogParser.new('game.log') }.to raise_error('File not found')
       end
     end
   end
-  
-  describe "#get_log_data" do
-    context "call print_log_data" do
-      it "prints lines number" do
-        log_parser = LogParser.new("spec/fixtures/game_test.log")
-        output = log_parser.get_log_data
+
+  describe '#log_data' do
+    context 'call print_log_data' do
+      it 'prints lines number' do
+        log_parser = LogParser.new('spec/fixtures/game_test.log')
+        output = log_parser.log_data
         expect(output).to start_with('"game_test.log":')
-        json_data = JSON.parse(output[17, output.length-1])
+        json_data = JSON.parse(output[17, output.length - 1])
         expect(json_data['lines']).to eq(52)
-        expect(json_data['players']).to eq(["Isgalamido", "<world>", "Dono da Bola", "Mocinha", "UnnamedPlayer"])
-        expect(json_data['kills']).to eq({"<world>"=>4, "Isgalamido"=>3})
+        expect(json_data['players']).to eq(['Isgalamido', '<world>', 'Dono da Bola', 'Mocinha', 'UnnamedPlayer'])
+        expect(json_data['kills']).to eq({ '<world>' => 4, 'Isgalamido' => 3 })
         expect(json_data['total_kills']).to eq(7)
       end
     end
